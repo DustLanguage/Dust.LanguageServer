51a52fe2eb72aea2305baae2acc0e5059610e3a4
Nufflee <zifcani@gmail.com>
2018-03-17 21:14:53 +0100

    Initial commit

---
+++ Dust.LanguageServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dust.LanguageServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
