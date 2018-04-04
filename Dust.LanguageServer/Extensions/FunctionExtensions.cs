5233c98ef879203b61b2997dbf99c1a0ab3fb189
Nufflee <zifcani@gmail.com>
2018-04-05 01:21:08 +0200

    Move property and function GetDetail to extensions.

---
+++ Dust.LanguageServer/Extensions/FunctionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Dust.Language.Nodes.Expressions;
+
+namespace Dust.LanguageServer.Extensions
+{
+  public static class FunctionExtensions
+  {
+    public static string GetDetail(this Function function)
+    {
+      StringBuilder builder = new StringBuilder($"let fn {function.Name}");
+
+      builder.Append("(");
+
+      if (function.Parameters.Length > 0)
+      {
+        for (int i = 0; i < function.Parameters.Length; i++)
+        {
+          builder.Append($"{function.Parameters[i].Identifier.Name}: any");
+
+          if (i != function.Parameters.Length - 1)
+          {
+            builder.Append(", ");
+          }
+        }
+      }
+
+      builder.Append("): any");
+
+      return builder.ToString();
+    }
+  }
+}
---
+++ Dust.LanguageServer/Extensions/IdentifierExpressionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Dust.Language.Nodes.Expressions;
+ 
+ namespace Dust.LanguageServer.Extensions
+ {
+   public static class IdentifierExpressionExtensions
+   {
+     public static string GetDetail(this IdentifierExpression property)
+     {
+       return $"let {(property.IsMutable ? "mut " : "")}{property.Name}: any";
+     }
+   }
+ }
